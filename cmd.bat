fb95273f1078ca1a7b0330ccb74af475f5af0ab4
Pramod Pallath Vasudevan <dev@pramodpallath.com>
2022-07-13 20:30:43 +0400

    cleaned old rnd

--- cmd.bat
+++
@@ -1,9 +0,0 @@
-npm install -g protractor
-REM install of protractor gives protractor + webdriver-manager
-protractor --version
-
-webdriver-manager update
-
-webdriver-manager start
-
-protractor conf.js
